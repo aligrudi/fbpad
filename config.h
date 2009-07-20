f956db55a82d7a1c669ed7c3c3ab4428a8183cf5
Ali Gholami Rudi <ali@rudi.ir>
2009-07-20 22:56:49 +0430

    config: use darker colors

--- config.h
+++ config.h
@@ -13,27 +13,27 @@
 
 /* black */
 #define COLOR0		0x0a0a0a
-#define COLOR8		0x71a3b7
+#define COLOR8		0x407080
 /* red */
-#define COLOR1		0xc04444
-#define COLOR9		0xc08888
+#define COLOR1		0xa02020
+#define COLOR9		0xb05050
 /* green */
-#define COLOR2		0x339933
-#define COLOR10		0x779977
+#define COLOR2		0x208020
+#define COLOR10		0x509050
 /* yellow */
-#define COLOR3		0xcccc66
-#define COLOR11		0xcccc99
+#define COLOR3		0x909050
+#define COLOR11		0xa0a070
 /* blue */
-#define COLOR4		0x5566bc
-#define COLOR12		0x8899bc
+#define COLOR4		0x202080
+#define COLOR12		0x505090
 /* magenta */
-#define COLOR5		0xcd66af
-#define COLOR13		0xcd99af
+#define COLOR5		0xa04080
+#define COLOR13		0xb06090
 /* cyan */
-#define COLOR6		0xa166cd
-#define COLOR14		0xa199cd
+#define COLOR6		0x8040a0
+#define COLOR14		0x9060b0
 /* white */
-#define COLOR7		0xeeeeee
+#define COLOR7		0xf0f0f0
 #define COLOR15		0xdedede
 
 /* framebuffer depth */
